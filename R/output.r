de6448136ec07d1afa09f209c219ae20973e7376
hadley <h.wickham@gmail.com>
2010-07-28 16:29:28 -0500

    Move rendering methods to own function

--- R/output.r
+++ R/output.r
@@ -41,44 +41,6 @@ static_file <- function(path, status = 200L) {
   )
 }
 
-#' Send output to browser
-#'
-#' @param text a character vector 
-#' @param status http status code
-#' @param mime_type mime type
-#' @param ... other named arguments become other http headers
-render <- function(text, status = 200L, mime_type = "text/html", ...) {
-  text <- stringr:str_c(text, collapse = "\n")
-  list(
-    payload = text, 
-    "content-type" = mime_type, 
-    "status code" = status,
-     ...)
-}
-
-#' Render template using brew.
-#' Templates are located in \code{views}.
-#' 
-#' @param template string giving template name (file name without extension)
-#' @param params list of parameters to be evaluated in template
-#' @param path web app path
-#' @export
-render_brew <- function(template, params = list(), path = getwd()) {
-  if (is.list(params)) {
-    env <- new.env(TRUE)
-    for(name in names(params)) {
-      env[[name]] <- params[[name]]
-    }
-    params <- env
-  }
-  
-  path <- file.path(path, "views", stringr::str_join(template, ".html"))
-  if (!file.exists(path)) stop("Can not find ", template, " template ",
-    call. = FALSE)
-
-  render(capture.output(brew::brew(path, envir = params)))
-}
-
 #' Redirect to new url.
 #'
 #' @param url full url to redirect to
